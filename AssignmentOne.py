4b02b172aa24ebb4c6e9122bd1ac6e86df6ef9a0
Brendan <banfieldb@carleton.edu>
2023-09-18 10:16:08 -0500

    added type hints to functions

--- AssignmentOne.py
+++ AssignmentOne.py
@@ -8,11 +8,11 @@ boards = {}
 gameId = 0
 
 @app.route("/newgame")
-def newGameHelp():
+def newGameHelp() -> str:
     return "Usage: http://localhost:5000/newgame/player, where player is x or o"
 
 @app.route("/newgame/<player>")
-def newgame(player):
+def newgame(player: str) -> str:
     player = player.lower()
     if player not in ['x', 'o']:
         return newGameHelp()
@@ -31,31 +31,32 @@ def newgame(player):
 def nextmoveHelp():
     return "Usage: http://localhost:5000/nextmove/gameID/row/col, where:<br>-gameID is a previously created game<br>-row and column are a legal move space"
 
-def getSquare(gameId, row, column):
+def getSquare(gameId: int, row: int, column: int) -> str:
     return boards[gameId][row * 19 + column + 2]
 
-def setSquare(gameId, row, column, newChar):
+def setSquare(gameId: int, row: int, column: int, newChar: str) -> None:
     idx = row * 19 + column + 2
     boards[gameId] = boards[gameId][:idx] + newChar + boards[gameId][idx + 1:]
 
-def isInBounds(row, col):
+def isInBounds(row: int, col: int) -> bool:
     if row < 0 or row >= 19:
         return False
     if col < 0 or col >= 19:
         return False
     return True
 
-def getTurn(gameId):
+def getTurn(gameId: int) -> None:
     return boards[gameId][0]
 
-def changeTurn(gameId):
+def changeTurn(gameId: int) -> None:
     turn = getTurn(gameId)
     if turn == 'x':
         boards[gameId] = 'o' + boards[gameId][1:]
     else:
         boards[gameId] = 'x' + boards[gameId][1:]
 
-def checkPattern(gameId, start: tuple, direction: tuple, pattern: list) -> bool:
+def checkPattern(gameId: int, start: tuple[int, int], direction: tuple[int, int], pattern: list) -> bool:
+    # key:
     # x and o: player characters
     # *: wildcard
     # |: border
@@ -79,13 +80,13 @@ def checkPattern(gameId, start: tuple, direction: tuple, pattern: list) -> bool:
     return True
         
 
-def getCaptures(gameId, player):
+def getCaptures(gameId: int, player: str) -> int:
     if player == 'x':
         return int(boards[gameId][2 + 19 * 19 + 1])
     else:
         return int(boards[gameId][2 + 19 * 19 + 3])
     
-def recordCapture(gameId, player):
+def recordCapture(gameId: int, player: str) -> None:
     if player == 'x':
         newScore = str(int(boards[gameId][-3]) + 1)
         boards[gameId] = boards[gameId][:-3] + newScore + boards[gameId][-2:]
@@ -93,14 +94,14 @@ def recordCapture(gameId, player):
         newScore = str(int(boards[gameId][-1]) + 1)
         boards[gameId] = boards[gameId][:-1] + newScore
 
-def checkCapture(gameId, row, col, direction, player, opponent):
+def checkCapture(gameId: int, row: int, col: int, direction: tuple[int, int], player: str, opponent: str) -> None:
     if checkPattern(gameId, (row, col), direction, [player, opponent, opponent, player]):
         dr, dc = direction
         setSquare(gameId, row + dr, col + dc, '-')
         setSquare(gameId, row + 2*dr, col + 2*dc, '-')
         recordCapture(gameId, player)
 
-def doCaptures(gameId, row, col, player):
+def doCaptures(gameId: int, row: int, col: int, player: str) -> None:
     opponent = 'o' if player == 'x' else 'x'
     for i in range(-1, 2):
         for j in range(-1, 2):
@@ -111,14 +112,14 @@ def doCaptures(gameId, row, col, player):
 
     
 
-def doMove(gameId, row, col):
+def doMove(gameId: int, row: int, col: int) -> None:
     turn = getTurn(gameId)
     setSquare(gameId, row, col, turn)
     doCaptures(gameId, row, col, turn)
     checkForFiveInARow(gameId, row, col, turn)
     changeTurn(gameId)
 
-def searchForPattern(gameId, pattern):
+def searchForPattern(gameId: int, pattern: list[str]) -> list[tuple[int, int]]:
     if "_" in pattern:
         moveIdx = pattern.index("_")
         pattern[moveIdx] = "-"
@@ -135,7 +136,7 @@ def searchForPattern(gameId, pattern):
                         captures.append((r + dr*moveIdx, c+dc*moveIdx))
     return captures
 
-def findGoodMoves(gameId):
+def findGoodMoves(gameId: int) -> list[tuple[int, int]]:
     p = getTurn(gameId)
     o = "x" if p == "o" else "o"
 
@@ -175,22 +176,22 @@ def findGoodMoves(gameId):
     
     
 
-def doComputerMove(gameId):
+def doComputerMove(gameId: int) -> None:
     moveChoices = findGoodMoves(gameId)
     move = moveChoices[random.randint(0, len(moveChoices)-1)]
     doMove(gameId, move[0], move[1])
 
-def getFormattedBoard(gameId):
+def getFormattedBoard(gameId: int) -> str:
     squares = boards[gameId][2:-4].replace("-", "_")
     lines = [" ".join(list(squares[19 * i: 19 * i + 19]) + [str(i+1)]) for i in range(19)]
     lines.append("0 " * 9 + "1 " * 10)
     lines.append(" ".join([str(i % 10) for i in range(1, 20)]))
     return "<br>".join(lines)
 
-def displayWin(gameId, player):
+def displayWin(gameId: int, player: str):
     raise NotImplementedError
 
-def checkForFiveInARow(gameId, row, col, player):
+def checkForFiveInARow(gameId: int, row: int, col: int, player: str) -> None:
     for i in range(-1, 2):
         for j in range(-1, 2):
             if i == 0 and j == 0: continue
@@ -198,7 +199,7 @@ def checkForFiveInARow(gameId, row, col, player):
                 displayWin(gameId, player)
  
 @app.route("/nextmove/<int:gameId>/<int:row>/<int:column>")
-def nextmove(gameId, row, column):
+def nextmove(gameId: int, row: int, column: int) -> str:
     row -= 1
     column -= 1
     global boards
